824628bbb326d6863f027c13cd6839eec6ce919d
zygisu <zygimantas.mercaitis@gmail.com>
2025-03-25 14:02:16 +0000

    Code almost finished. Atribute error found. Looking for solution

--- war.py
+++ war.py
@@ -1,10 +1,12 @@
+import random
+
 class Suit:
 
-    symbols = {"clubs":"♣", "diamonds":"♦", "hearts":"♥", "spades":"♠"} #dict. with keys and symbols
+    SYMBOLS = {"clubs":"♣", "diamonds":"♦", "hearts":"♥", "spades":"♠"} #dict. with keys and symbols
 
     def __init__(self, description): #creates a object with description and symbols 
         self._description = description
-        self._symbol = Suit.symbols[description.lower()] #Suit. - name of class. symbols key of description 
+        self._symbol = Suit.SYMBOLS[description.lower()] #Suit. - name of class. symbols key of description 
 
     @property #getters becouse those cant be modified outside the class 
     def description(self):
@@ -44,4 +46,195 @@ class Card:
     def is_special(self):
         return self._value >= 11
 
-# linkejimai
+class Deck:
+
+    SUITS = ("clubs", "diamonds", "hearts", "spades")
+
+    def __init__(self, is_empty=False): 
+        self._cards = [] #empty list of cards
+
+        if not is_empty:
+            self.build() #creates a deck using def build
+
+    @property
+    def size(self):
+        return len(self._cards)
+    
+    def build(self):
+        for suit in Deck.SUITS: # 4 x 14 (4 suits, 14 card values)
+            for value in range(2, 15):
+                self._cards.append(Card(Suit(suit), value))
+    
+    def show(self):
+        for card in self._cards:
+            card.show()
+
+    def shuffle(self):
+        random.shuffle(self._cards)
+
+    def draw(self):
+        if self._cards:
+            return self._cards.pop() #returns last card from the list .pop takes last argument from the list
+        else: 
+            return None
+        
+    def add(self, card):
+        self._cards.insert(0, card)
+
+class Player:
+    
+    def __init__(self, name, deck, is_computer=False):
+        self.name = name
+        self._deck = deck
+        self._is_computer = is_computer
+
+    @property
+    def is_computer(self):
+        return self._is_computer
+    
+    @property
+    def deck(self):
+        return self._deck
+    
+    def has_empty_deck(self):
+        return self._deck.size == 0 
+    
+    def draw_card(self):
+        if not self.has_empty_deck():
+            return self._deck.draw()
+        else:
+            return None
+    
+    def add_card(self, card):
+        self._deck.add(card)
+
+class Game:
+
+    PLAYER = 0
+    COMPUTER = 1
+    TIE = 2
+
+    def __init__(self, player, computer, deck):
+        self._player = player
+        self._computer = computer
+        self._deck = deck
+
+        self.make_initial_decks()
+
+    def make_initial_decks(self):
+        self._deck.shuffle()
+        self.make_deck(self._player)
+        self.make_deck(self._computer)
+
+    def make_deck(self, character): #character - player or computer
+        for i in range(26):
+            card = self._deck.draw()
+            character.add_card(card)
+    
+    def start_battle(self, cards_from_war=None):
+
+        print("\n== Let`s Start the Battle ==\n")
+
+        player_card = self._player.draw_card()
+        computer_card = self._computer.draw_card()
+
+        print(f"Your Card: ")
+        player_card.show()
+
+        print(f"\nComputer Card: ")
+        computer_card.show()
+
+        winner = self.get_round_winner(player_card, computer_card)
+        cards_won = self.get_cards_won(player_card, computer_card, cards_from_war)
+
+        if winner == Game.PLAYER:
+            print("\nYou won this round!")
+            self.add_cards_to_character(self._player, cards_won)
+        elif winner == Game. COMPUTER:
+            print("\nComputer won this round...")
+            self.add_cards_to_character(self._computer, cards_won)
+        else:
+            print("\nIt`s a tie. War starts!")
+            self.start_war(cards_won)
+
+        return winner
+    
+    def get_round_winner(self, player_card, computer_card):
+        if player_card.value > computer_card.value:
+            return Game.PLAYER
+        elif player_card.value < computer_card.value:
+            return Game.COMPUTER
+        else:
+            return Game.TIE
+    
+    def get_cards_won(self, player_card, computer_card, previous_cards):
+        if previous_cards:
+            return [player_card, computer_card] + previous_cards
+        else:
+            return [player_card, computer_card]
+
+    def add_cards_to_character(self, character, list_of_cards):
+        for card in list_of_cards:
+            character.add_card(card)
+    
+    def start_war(self, cards_from_battle):
+        player_cards = []
+        computer_cards = []
+
+        for i in range(3):
+            player_cards = self._player.draw_card()
+            player_cards.append()
+
+
+            computer_card = self._computer.draw_card()
+            computer_cards.append()
+
+        print("Six hidden cards: XXX XXX")
+
+        self.start_battle(player_cards + computer_cards + cards_from_battle)
+
+    def check_game_over(self):
+        if self._player.has_empty_deck():
+            print("=====================")
+            print("      GAME OVER      ")
+            print("  COMPUTER WON...    ")
+            print("=====================")
+            return True
+        elif self._computer.has_empty_deck():
+            print("=====================")
+            print("      GAME OVER      ")
+            print("       YOU WON!      ")
+            print("=====================")
+            return True
+        else:
+            return False
+        
+    def print_stats(self):
+        print("\n----")
+        print(f"You have {self._player.deck.size} cards on your deck.")
+        print(f"The computer has {self._computer.deck.size} cards in its deck.")
+        print("----")
+
+    def print_welcome_message(self):
+        print("=====================")
+        print("    War Card Game    ")
+        print("=====================")
+
+
+player = Player("Dude", Deck(is_empty=True))
+computer = Player("Computer", Deck(is_empty=True), is_computer=True)
+deck = Deck()
+
+game = Game(player, computer, deck)
+
+game.print_welcome_message()
+
+while not game.check_game_over():
+    game.start_battle()
+    game.print_stats()
+
+    answer = input("\nAre you ready for next round?\nPress Enter to continue.\nPress X to stop")
+
+    if answer.lower() == "x":
+        break
+
